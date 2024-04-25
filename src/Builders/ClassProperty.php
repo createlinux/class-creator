8903575d6a471c6412bf362c88d235c2b83c501a
Administrator <howmao@qq.com>
2024-04-25 17:39:48 +0800

    创建属性

--- src/Builders/ClassProperty.php
+++ src/Builders/ClassProperty.php
@@ -2,6 +2,8 @@
 
 namespace Createlinux\ClassCreator\Builders;
 
+use Createlinux\ClassCreator\Builders\Basic\VisibilityIdentify;
+
 /**
  * @class 类属性
  *
@@ -9,4 +11,39 @@ namespace Createlinux\ClassCreator\Builders;
 class ClassProperty
 {
     //TODO
+    private string $name;
+    private ?VisibilityIdentify $visibility;
+    //TODO 类型
+
+    public function __construct(string $name, VisibilityIdentify $visibility = null)
+    {
+
+        $this->name = $name;
+        $this->visibility = $visibility;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getVisibility(): ?VisibilityIdentify
+    {
+        return $this->visibility;
+    }
+
+    public function isPublic()
+    {
+        return $this->visibility->name === 'public';
+    }
+
+    public function isPrivate()
+    {
+        return $this->visibility->name === 'private';
+    }
+
+    public function isProtected()
+    {
+        return $this->visibility->name === 'protected';
+    }
 }
