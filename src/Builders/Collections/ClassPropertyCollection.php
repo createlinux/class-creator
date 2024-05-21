54b39433a6f9c58290c3c35ab2ac49f866ea30e4
Administrator <howmao@qq.com>
2024-05-21 18:46:36 +0800

    增加put

--- src/Builders/Collections/ClassPropertyCollection.php
+++ src/Builders/Collections/ClassPropertyCollection.php
@@ -3,9 +3,20 @@
 namespace Createlinux\ClassCreator\Builders\Collections;
 
 use Createlinux\ClassCreator\Builders\Abstract\CollectionAbstract;
+use Createlinux\ClassCreator\Builders\ClassProperty;
 use Illuminate\Support\Collection;
 
 class ClassPropertyCollection extends CollectionAbstract
 {
-    //TODO 属性
+    /**
+     *
+     * put属性
+     * @param string $name
+     * @param ClassProperty $classProperty
+     * @return Collection
+     */
+    public function put(string $name, ClassProperty $classProperty)
+    {
+        return $this->getItems()->put($name,$classProperty);
+    }
 }
