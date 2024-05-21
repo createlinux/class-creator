531c6c388cea214f83b58e547e68de5d1a9630fc
Administrator <howmao@qq.com>
2024-05-21 18:46:25 +0800

    增加返回值

--- src/Builders/ClassProperty.php
+++ src/Builders/ClassProperty.php
@@ -3,6 +3,8 @@
 namespace Createlinux\ClassCreator\Builders;
 
 use Createlinux\ClassCreator\Builders\Basic\VisibilityIdentify;
+use Createlinux\ClassCreator\Builders\Collections\DataTypeCollection;
+use Illuminate\Support\Str;
 
 /**
  * @class 类属性
@@ -13,13 +15,22 @@ class ClassProperty
     //TODO
     private string $name;
     private ?VisibilityIdentify $visibility;
-    //TODO 类型
+
+    /**
+     * TODO 类型
+     * 如果是object类型，则需要调用usingClass引入类，并且在构造函数内初始化类(或者设置该参数为nullable)
+     * @var DataTypeCollection
+     */
+    private DataTypeCollection $dataType;
 
     public function __construct(string $name, VisibilityIdentify $visibility = null)
     {
-
-        $this->name = $name;
+        if(!$name){
+            throw new \InvalidArgumentException("参数1不能为空");
+        }
+        $this->name = Str::camel(to_singular_name($name));
         $this->visibility = $visibility;
+        $this->dataType = new DataTypeCollection();
     }
 
     public function getName(): string
@@ -46,4 +57,16 @@ class ClassProperty
     {
         return $this->visibility->name === 'protected';
     }
+
+    /**
+     *
+     * 属性数据类型
+     * @return DataTypeCollection
+     */
+    public function getDataType(): DataTypeCollection
+    {
+        return $this->dataType;
+    }
+
+
 }
